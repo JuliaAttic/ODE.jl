ceb0632ba47c11b0ad766efa9594d4c8969fb6e1
ChrisRackauckas <me@chrisrackauckas.com>
2016-12-28 00:43:36 -0800

    update to review

--- src/common.jl
+++ src/common.jl
@@ -12,86 +12,83 @@ immutable feh45 <: ODEIterAlgorithm end
 typealias KW Dict{Symbol,Any}
 
 function solve{uType,tType,isinplace,algType<:ODEIterAlgorithm,F}(prob::AbstractODEProblem{uType,tType,isinplace,F},
-    alg::algType,timeseries=[],ts=[],ks=[];dense=true,save_timeseries=true,
-    saveat=[],callback=()->nothing,timeseries_errors=true,dense_errors=false,
-    kwargs...)
-  tspan = prob.tspan
+      alg::algType,timeseries=[],ts=[],ks=[];dense=true,save_timeseries=true,
+      saveat=[],callback=()->nothing,timeseries_errors=true,dense_errors=false,
+      kwargs...)
 
-  if tspan[end]-tspan[1]<tType(0)
-    error("final time must be greater than starting time. Aborting.")
-  end
-  atomloaded = isdefined(Main,:Atom)
-  o = KW(kwargs)
-  t = tspan[1]
-  u0 = prob.u0
-  o[:T] = tspan[end]
+    tspan = prob.tspan
 
-  if typeof(u0) <: Number
-    u = [u0]
-  else
-    u = deepcopy(u0)
-  end
+    o = KW(kwargs)
+    t = tspan[1]
+    u0 = prob.u0
+    o[:T] = tspan[end]
 
-  sizeu = size(u)
-
-  opts = buildOptions(o,ODEJL_OPTION_LIST,ODEJL_ALIASES,ODEJL_ALIASES_REVERSED)
-
-  if !isinplace && typeof(u)<:AbstractArray
-    f! = (t,u,du) -> (du[:] = prob.f(t,u))
-  else
-    f! = prob.f
-  end
-  ode  = ODE.ExplicitODE(t,u,f!)
-  # adaptive==true ? FoA=:adaptive : FoA=:fixed #Currently limied to only adaptive
-  FoA = :adaptive
-  if typeof(alg) <: rk23
-    solver = ODE.RKIntegrator{FoA,:rk23}
-  elseif typeof(alg) <: rk45
-    solver = ODE.RKIntegrator{FoA,:dopri5}
-  elseif typeof(alg) <: feh78
-    solver = ODE.RKIntegrator{FoA,:feh78}
-  elseif typeof(alg) <: ModifiedRosenbrock
-    solver = ODE.ModifiedRosenbrockIntegrator
-  elseif typeof(alg) <: feuler
-    solver = ODE.RKIntegratorFixed{:feuler}
-  elseif typeof(alg) <: midpoint
-    solver = ODE.RKIntegratorFixed{:midpoint}
-  elseif typeof(alg) <: heun
-    solver = ODE.RKIntegratorFixed{:heun}
-  elseif typeof(alg) <: rk4
-    solver = ODE.RKIntegratorFixed{:rk4}
-  elseif typeof(alg) <: feh45
-    solver = ODE.RKIntegrator{FoA,:rk45}
-  end
-  out = ODE.solve(ode;solver=solver,opts...)
-  timeseries = out.y
-  ts = out.t
-  ks = out.dy
-  if length(out.y[1])==1
-    tmp = Vector{eltype(out.y[1])}(length(out.y))
-    tmp_dy = Vector{eltype(out.dy[1])}(length(out.dy))
-    for i in 1:length(out.y)
-      tmp[i] = out.y[i][1]
-      tmp_dy[i] = out.dy[i][1]
+    if typeof(u0) <: Number
+        u = [u0]
+    else
+        u = deepcopy(u0)
     end
-    timeseries = tmp
-    ks = tmp_dy
-  end
 
-  saveat_idxs = find((x)->x∈saveat,ts)
-  t_nosaveat = view(ts,symdiff(1:length(ts),saveat_idxs))
-  u_nosaveat = view(timeseries,symdiff(1:length(ts),saveat_idxs))
+    sizeu = size(u)
 
-  if dense
-    interp = (tvals) -> common_interpolation(tvals,t_nosaveat,u_nosaveat,ks,alg,f!)
-  else
-    interp = (tvals) -> nothing
-  end
+    opts = buildOptions(o,ODEJL_OPTION_LIST,ODEJL_ALIASES,ODEJL_ALIASES_REVERSED)
 
-  build_solution(prob,alg,ts,timeseries,
-                    dense=dense,k=ks,interp=interp,
-                    timeseries_errors = timeseries_errors,
-                    dense_errors = dense_errors)
+    if !isinplace && typeof(u)<:AbstractArray
+        f! = (t,u,du) -> (du[:] = prob.f(t,u))
+    else
+        f! = prob.f
+    end
+    ode  = ODE.ExplicitODE(t,u,f!)
+    # adaptive==true ? FoA=:adaptive : FoA=:fixed #Currently limied to only adaptive
+    FoA = :adaptive
+    if algType <: rk23
+        solver = ODE.RKIntegrator{FoA,:rk23}
+    elseif algType <: rk45
+        solver = ODE.RKIntegrator{FoA,:dopri5}
+    elseif algType <: feh78
+        solver = ODE.RKIntegrator{FoA,:feh78}
+    elseif algType <: ModifiedRosenbrock
+        solver = ODE.ModifiedRosenbrockIntegrator
+    elseif algType <: feuler
+        solver = ODE.RKIntegratorFixed{:feuler}
+    elseif algType <: midpoint
+        solver = ODE.RKIntegratorFixed{:midpoint}
+    elseif algType <: heun
+        solver = ODE.RKIntegratorFixed{:heun}
+    elseif algType <: rk4
+        solver = ODE.RKIntegratorFixed{:rk4}
+    elseif algType <: feh45
+        solver = ODE.RKIntegrator{FoA,:rk45}
+    end
+    out = ODE.solve(ode;solver=solver,opts...)
+    timeseries = out.y
+    ts = out.t
+    ks = out.dy
+    if length(out.y[1])==1
+        tmp = Vector{eltype(out.y[1])}(length(out.y))
+        tmp_dy = Vector{eltype(out.dy[1])}(length(out.dy))
+        for i in 1:length(out.y)
+            tmp[i] = out.y[i][1]
+            tmp_dy[i] = out.dy[i][1]
+        end
+        timeseries = tmp
+        ks = tmp_dy
+    end
+
+    saveat_idxs = find((x)-> x ∈ saveat,ts)
+    t_nosaveat = view(ts,symdiff(1:length(ts),saveat_idxs))
+    u_nosaveat = view(timeseries,symdiff(1:length(ts),saveat_idxs))
+
+    if dense
+        interp = (tvals) -> common_interpolation(tvals,t_nosaveat,u_nosaveat,ks,alg,f!)
+    else
+        interp = (tvals) -> nothing
+    end
+
+    build_solution(prob,alg,ts,timeseries,
+                      dense=dense,k=ks,interp=interp,
+                      timeseries_errors = timeseries_errors,
+                      dense_errors = dense_errors)
 end
 
 const ODEJL_OPTION_LIST = Set([:tout,:tstop,:reltol,:abstol,:minstep,:maxstep,:initstep,:norm,:maxiters,:isoutofdomain])
@@ -99,9 +96,9 @@ const ODEJL_ALIASES = Dict{Symbol,Symbol}(:minstep=>:dtmin,:maxstep=>:dtmax,:ini
 const ODEJL_ALIASES_REVERSED = Dict{Symbol,Symbol}([(v,k) for (k,v) in ODEJL_ALIASES])
 
 function buildOptions(o,optionlist,aliases,aliases_reversed)
-  dict1 = Dict{Symbol,Any}([Pair(k,o[k]) for k in (keys(o) ∩ optionlist)])
-  dict2 = Dict([Pair(aliases_reversed[k],o[k]) for k in (keys(o) ∩ values(aliases))])
-  merge(dict1,dict2)
+    dict1 = Dict{Symbol,Any}([Pair(k,o[k]) for k in (keys(o) ∩ optionlist)])
+    dict2 = Dict([Pair(aliases_reversed[k],o[k]) for k in (keys(o) ∩ values(aliases))])
+    merge(dict1,dict2)
 end
 
 """
--- test/common.jl
+++ test/common.jl
@@ -1,66 +1,26 @@
-using DiffEqProblemLibrary, DiffEqBase
+using DiffEqProblemLibrary, DiffEqBase, ODE
 
 prob = prob_ode_linear
 dt=1/2^(4)
 
-sol =solve(prob,feuler();dt=dt)
-#plot(sol,plot_analytic=true)
+algs = [feuler(),rk23(),rk45(),feh78(),ModifiedRosenbrock(),midpoint(),heun(),rk4(),feh45()]
 
-sol =solve(prob,rk23(),dt=dt)
-
-sol =solve(prob,rk45(),dt=dt)
-
-sol =solve(prob,feh78(),dt=dt)
-
-sol =solve(prob,ModifiedRosenbrock(),dt=dt)
-
-sol =solve(prob,midpoint(),dt=dt)
-
-sol =solve(prob,heun(),dt=dt)
-
-sol =solve(prob,rk4(),dt=dt)
-
-sol =solve(prob,feh45(),dt=dt)
+for alg in algs
+    sol = solve(prob,alg;dt=dt)
+end
 
 prob = prob_ode_2Dlinear
 
-sol =solve(prob,feuler(),dt=dt)
+for alg in algs
+    if alg != ModifiedRosenbrock() #ODE.jl issues with 2D
+      sol = solve(prob,alg;dt=dt)
+    end
+end
 
-sol =solve(prob,rk23(),dt=dt)
-
-sol =solve(prob,rk45(),dt=dt)
-
-sol =solve(prob,feh78(),dt=dt)
-
-#sol =solve(prob,ModifiedRosenbrock(),dt=dt) #ODE.jl issues with 2D
-
-sol =solve(prob,midpoint(),dt=dt)
-
-sol =solve(prob,heun(),dt=dt)
-
-sol =solve(prob,rk4(),dt=dt)
-
-sol =solve(prob,feh45(),dt=dt)
-
-#=
 prob = prob_ode_bigfloat2Dlinear
 
-sol =solve(prob,feuler(),dt=dt)
-TEST_PLOT && plot(sol,plot_analytic=true)
-
-sol =solve(prob,rk23(),dt=dt)
-
-sol =solve(prob,rk4()5,dt=dt)
-
-sol =solve(prob,feh78(),dt=dt)
-
-#sol =solve(prob,dt=0,alg=:ode23s) #ODE.jl issues
-
-sol =solve(prob,midpoint(),dt=dt)
-
-sol =solve(prob,heun(),dt=dt)
-
-sol =solve(prob,rk4(),dt=dt)
-
-sol =solve(prob,feh45(),dt=dt)
-=#
+for alg in algs
+    if alg != ModifiedRosenbrock() #ODE.jl issues with 2D
+        sol = solve(prob,alg;dt=dt)
+    end
+end
